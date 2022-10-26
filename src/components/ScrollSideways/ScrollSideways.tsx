12401988db51991c1d90cf18bc2bfb31ee387cbb
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-10-26 08:22:33 +0300

    Moved functionality in hook

--- src/components/ScrollSideways/ScrollSideways.tsx
+++ src/components/ScrollSideways/ScrollSideways.tsx
@@ -1,13 +1,7 @@
-import {
-  useReducedMotion,
-  useScroll,
-  useTransform,
-  useSpring,
-  motion,
-} from "framer-motion";
-import { FunctionComponent, useRef, useState } from "react";
+import { useReducedMotion, motion } from "framer-motion";
+import { FunctionComponent, useRef } from "react";
 import ScrollSidewaysProps from "./ScrollSidewaysProps";
-import useIsomorphicLayoutEffect from "@/hooks/useIsomorphicLayoutEffect";
+import useScrollSideways from "./hooks/useScrollSideways";
 
 const ScrollSideways: FunctionComponent<ScrollSidewaysProps> = (props) => {
   const {
@@ -18,37 +12,9 @@ const ScrollSideways: FunctionComponent<ScrollSidewaysProps> = (props) => {
     initialX = 0,
   } = props;
   const prefersReducedMotion = useReducedMotion();
-  const [elementTop, setElementTop] = useState(0);
-  const [clientHeight, setClientHeight] = useState(0);
   const ref = useRef<HTMLDivElement>(null);
 
-  const { scrollY } = useScroll();
-
-  const initial = Math.max(elementTop - clientHeight, 0);
-  const final = elementTop + offset;
-
-  const directionValue = direction === "left" ? -1 : 1;
-
-  const xRange = useTransform(
-    scrollY,
-    [initial, final],
-    [initialX, offset * directionValue]
-  );
-  const x = useSpring(xRange, { stiffness: 400, damping: 90 });
-
-  useIsomorphicLayoutEffect(() => {
-    const element = ref.current;
-    const onResize = () => {
-      if (element)
-        setElementTop(
-          element.getBoundingClientRect().top + window.scrollY || window.scrollY
-        );
-      setClientHeight(window.innerHeight);
-    };
-    onResize();
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
-  }, [ref]);
+  const x = useScrollSideways(ref, direction, offset, initialX);
 
   if (prefersReducedMotion || !isEffectActive) {
     return <>{children}</>;
