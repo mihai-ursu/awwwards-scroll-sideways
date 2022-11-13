4966ae3160ee67973d50be4e18a5bec73db5b23f
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-13 15:20:23 +0200

    Removed Contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./styles/reset.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
-import Contact from "./page/Contact";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
